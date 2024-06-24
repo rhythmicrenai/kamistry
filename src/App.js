671170c6c3eff395314dbfe4ace98c26e17065eb
rhythmicrenai <165713237+rhythmicrenai@users.noreply.github.com>
2024-06-24 13:11:39 -0700

    new grid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+  return 
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>;
 }
 
-export default function MyApp() {
+export default function Board() {
   return (
-    <div>
-      <h1>Kamistry is the Science of Life</h1>
-      <MyButton />
+    <>
+    <div className="board-row">
+      <Square value="1"/>
+      <Square value="2"/>
+      <Square value="3"/>
     </div>
+    <div className="board-row">
+      <Square value="4"/>
+      <Square value="5"/>
+      <Square value="6"/>
+    </div>
+    <div className="board-row">
+      <Square value="7"/>
+      <Square value="8"/>
+      <Square value="9"/>
+    </div>
+    </>
   );
 }
